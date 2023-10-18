935b93a4a7d29a51186f2dd1b703933c40b317d3
DahbOne1 <david.arcos1000@gmail.com>
2023-10-18 12:15:13 -0600

    multilinguage

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const contBand = document.getElementById("cont-band");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) =>{
+    const requestJson = await fetch(`./languages/${language}Index.json`);
+    const texts = await requestJson.json();
+    
+    for(const textToChange of textsToChange){
+       const section = textToChange.dataset.section;
+       const value = textToChange.dataset.value;
+
+       textToChange.innerHTML = texts[section][value];
+    }
+}
+
+contBand.addEventListener("click", (e) => {
+    changeLanguage(e.target.dataset.language);
+});
